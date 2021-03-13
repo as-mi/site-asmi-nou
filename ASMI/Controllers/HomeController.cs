29e1a3bf6f2cbe72c2a242b92ff0b8a9f44f96a7
GeorgeBodea <georgebodeacn@yahoo.com>
2021-03-13 21:47:06 +0200

    Schita site
    Am adaugat meniu-ul de la site si paginile care le va contine. (deocamdata)

---
+++ ASMI/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using ASMI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASMI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Departamente()
+        {
+            return View();
+        }
+
+        public IActionResult Smarthack()
+        {
+            return View();
+        }
+
+        public IActionResult GhidulBobocului()
+        {
+            return View();
+        }
+
+        public IActionResult CalendarASMI()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
