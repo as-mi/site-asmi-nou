a08dea894d6631108865da24abc22d638c989d28
Reznicencu Bogdan <reznicencu.bogdan99@gmail.com>
2021-05-12 16:59:15 +0300

    Fix - nginx compliant

--- ASMI/Startup.cs
+++ ASMI/Startup.cs
@@ -36,10 +36,8 @@ namespace ASMI
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
