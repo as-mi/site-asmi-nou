16f620b9d97e212e93b6eb3f0f820de4906da1e2
Micuda Andrei <andrei.micuda@gmail.com>
2021-09-29 19:24:23 +0300

    Bunch of changes

--- ASMI/wwwroot/js/site.js
+++ ASMI/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    var backToTopButton = $('#backToTopButton');
 
-// Write your JavaScript code.
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            backToTopButton.addClass('show');
+        } else {
+            backToTopButton.removeClass('show');
+        }
+    });
+
+    backToTopButton.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+});
