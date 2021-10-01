9131b439123c547228d24e3e5964a6cd22984ad6
Micuda Andrei <andrei.micuda@gmail.com>
2021-10-01 19:45:16 +0300

    Implementat galerie foto

--- ASMI/Controllers/HomeController.cs
+++ ASMI/Controllers/HomeController.cs
@@ -223,6 +223,10 @@ namespace ASMI.Controllers
         public IActionResult Studenti()
         {
             return View();
+        }  
+        public IActionResult Galerie()
+        {
+            return View();
         }     
     }
 }
