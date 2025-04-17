bf372305375501ab8dc896ed916e7e4db1eebf76
mihaidanaila11 <danailateodor@yahoo.com>
2025-04-18 01:12:04 +0300

    Carduri Proiecte

---
+++ ASMI/Models/Proiect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ASMI.Models
+{
+	public class Proiect
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime? Date { get; set; }
+
+		public string WebSite { get; set; }
+
+		public Proiect(string Name, string Description, DateTime? Date, string WebSite) { 
+			this.Name = Name;
+			this.Description = Description;
+			this.Date = Date;
+			this.WebSite = WebSite;
+		}
+	}
+
+}
